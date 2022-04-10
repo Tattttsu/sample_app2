f9d6543d2a45bfeb6516d4153ade1f2983cebe1c
Ubuntu <ubuntu@ip-172-31-45-28.ap-northeast-3.compute.internal>
2022-04-10 12:53:05 +0000

    5章の終わり

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,8 @@
 Rails.application.routes.draw do
-  root "static_page#home"
-  get 'static_page/home'
-  get 'static_page/help'
-  get 'static_page/about'
-  get 'static_page/contact'
-  
-  
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  
+  get 'users/new'
+  root 'static_page#home'
+  get  '/help',    to: 'static_page#help'
+  get  '/about',   to: 'static_page#about'
+  get  '/contact', to: 'static_page#contact'
+  get "/signup", to: "users#new"
 end
