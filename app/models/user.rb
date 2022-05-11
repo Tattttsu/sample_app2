a4772805cf826d7cda7bd531554e6906a5d9dc02
Ubuntu <ubuntu@ip-172-31-45-28.ap-northeast-3.compute.internal>
2022-05-11 13:37:45 +0000

    9章の終わり

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  attr_accessor :remember_token
+  
   before_save { self.email = email.downcase }
   validates :name, presence: true, length: {maximum: 50}
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
@@ -12,5 +14,27 @@ class User < ApplicationRecord
                                                   BCrypt::Engine.cost
     BCrypt::Password.create(string, cost: cost)
   end
+    
+    #　ランダムなトークンを返す
+  def User.new_token
+    SecureRandom.urlsafe_base64
+  end
+  
+     #userのremember_tokenにトークンを入れ、remember_digestにハッシュ化して記憶
+  def remember
+    self.remember_token = User.new_token
+    update_attribute(:remember_digest, User.digest(remember_token))
+  end
+  
+   #ハッシュ化されたパスと一致したらtrue返す
+  def authenticated?(remember_token)
+    return false if remember_digest.nil?
+    BCrypt::password.new(remember_digest).is_password?(remember_token)
+  end
+  
+  def forget
+    update_attribute(:remember_digest, nil)
+  end
+  
   
 end
